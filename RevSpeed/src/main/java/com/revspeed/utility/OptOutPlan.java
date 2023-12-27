76cb0d55e18929e75d367441b4d03f14708c3931
kczodape <krunalzodape9@gmail.com>
2023-12-27 23:10:21 +0530

    Updated login testing

--- RevSpeed/src/main/java/com/revspeed/utility/OptOutPlan.java
+++ RevSpeed/src/main/java/com/revspeed/utility/OptOutPlan.java
@@ -19,7 +19,7 @@ public class OptOutPlan {
     }
 
     public static void optOutPlan(String todaysDate) throws SQLException {
-        String deactivateUserQuerry = "update user_service_link set user_status = 0 where subscription_end_date = '?'";
+        String deactivateUserQuerry = "update user_service_link set user_status = 0 where subscription_end_date = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(deactivateUserQuerry);
         preparedStatement.setString(1, todaysDate);
         preparedStatement.executeUpdate();
--- RevSpeed/src/test/java/com/revspeed/services/UserServiceTest.java
+++ RevSpeed/src/test/java/com/revspeed/services/UserServiceTest.java
@@ -39,4 +39,5 @@ public class UserServiceTest {
 //        Assert
         Mockito.verify(mockedUserDao, Mockito.times(1)).loginUser();
     }
+
 }
